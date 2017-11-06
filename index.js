5c7c0e5e640a55eae35420d652b3a38fd9e499fd
swust-xiaoj <swustxiaojie@163.com>
2017-11-06 11:29:28 +0800

    add github markdown css

--- index.js
+++ index.js
@@ -15,19 +15,21 @@ const getMD = (dir, cb) => {
 };
 
 const markit = (src, cb) => {
-    let basename, truepath, content, html, htmlpath;
+    let basename, truepath, content, html, htmlpath, template;
     basename = path.basename(src, '.md');
     truepath = path.resolve(__dirname, src);
-    htmlpath = path.resolve(__dirname, basename + '.html');
+    htmlpath = path.resolve(__dirname, 'dist', basename + '.html');
     content = fs.readFileSync(truepath, 'utf-8');
     html = marked(content);
-    pdf.create(html, { format: 'A4' }).toFile(path.resolve(__dirname, basename + '.pdf'), (err, res) => {
+    console.log(htmlpath + ' finished.')
+    template = `<meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">\n<link href="https://cdn.bootcss.com/github-markdown-css/2.8.0/github-markdown.min.css" rel="stylesheet">\n<article class='markdown-body'>\n${html}</article>`;
+    pdf.create(template, { format: 'A4' }).toFile(path.resolve(__dirname, 'dist', basename + '.pdf'), (err, res) => {
         if (err) {
             throw err;
         }
         console.log(res.filename + ' finished.');
     });
-    cb(htmlpath, html);
+    cb(htmlpath, template);
 };
 
 getMD('./bin/md', pathname => {
